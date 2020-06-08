c83532459dea89d4b97bfd2f98f9149fc524abde
= <navneil2006@gmail.com>
2020-06-08 18:58:52 +1000

    JSON Web Tokens Completed

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ import os
 from User import User
 from Mail import Mail
 from SMS import SMS
+from functools import wraps
+
+import jwt
+import datetime
 
 # Create Flask Object
 app = Flask(__name__)
@@ -11,6 +15,25 @@ app.config['JSON_SORT_KEYS'] = False
 app.secret_key = os.environ['SECRET_KEY']
 
 users_list = []
+
+# JWT 
+def jwt_required(func):
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        token = request.args.get('token')
+
+        # Missing Token
+        if not token:
+            return jsonify({'message': 'Missing Token'}), 400
+        
+        try:
+            data = jwt.decode(token, os.environ['SECRET_KEY'])
+        except:
+            return jsonify({'message': 'Invalid Token Supplied'}), 400
+
+        return func(*args, **kwargs)
+
+    return decorated
 
 # Create the Endpoints
 @app.route('/')
@@ -43,14 +66,23 @@ def login():
 
         for user in users_list:
             if username == user.name and password == user.password:
-                mail = Mail(user.mail, 'Login', 'Someone Logged into your account...')
+                token = jwt.encode({'name': user.name, 'exp': datetime.datetime.utcnow()+datetime.timedelta(minutes=15)}, os.environ['SECRET_KEY'])
+
+                mail = Mail(user.mail, 'Login', 'http://0.0.0.0:50/user-login?token={}'.format(token.decode('utf-8')))
                 mail.send()
                 sms = SMS(user.phone, 'Someone Logged into your account')
                 sms.send()
-                return jsonify({'message': 'Working'}), 200
+                return jsonify({'token': token.decode('utf-8')}), 200
             else:
                 return jsonify({'message': 'Username or Password is Invalid'}), 400
         return jsonify({'message': 'Username or Password is Invalid'}), 400      
+
+@app.route('/user-login')
+@jwt_required
+def user_login():
+    return jsonify({
+        'message': 'System is Completed'
+    }), 200
 
 # Run the App
 if __name__ == '__main__':
